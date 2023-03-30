1c1cb6ccd6144fe1537cbc8846538a1b4fa8424f
Suleman <89522275+Suleman-Sarwar@users.noreply.github.com>
2023-03-30 12:27:29 +0100

    Update AD-Generator.ps1

--- AD-Generator.ps1
+++ AD-Generator.ps1
@@ -1,29 +1,50 @@
-# Set variables for user and OU creation
-$ouPrefix = "TestOU"
-$userPrefix = "TestUser"
-$domainName = "yourdomain.com"
-$numberOfOUs = 5
-$numberOfUsers = 100
+ # Load ActiveDirectory module
+Import-Module ActiveDirectory
 
-# Create OUs
-for ($i = 1; $i -le $numberOfOUs; $i++) {
-    $ouName = $ouPrefix + $i
-    New-ADOrganizationalUnit -Name $ouName -Path "DC=$domainName" -ErrorAction SilentlyContinue
-}
+# Set variables for user creation
+$ouPath = "OU=Test,DC=domain,DC=local"
+$firstNameList = "John","Jane","David","Michael","Sarah","Emily","Jessica","Amanda","Jacob","William","Olivia","Noah","Sophia","Liam","Mia","Ava","Ethan","Isabella","Jackson","Charlotte"
+$lastNameList = "Smith","Johnson","Brown","Taylor","Davis","Wilson","Anderson","Thomas","Jackson","Lee","Garcia","Martinez","Davis","Parker","Hill","Cooper","Reed","Cox","Ward","Foster"
 
-# Create random users in random OUs
-for ($i = 1; $i -le $numberOfUsers; $i++) {
-    $ouNumber = Get-Random -Minimum 1 -Maximum $numberOfOUs
-    $ouName = $ouPrefix + $ouNumber
-    $userFirstName = Get-Random -InputObject @("John", "Jane", "Michael", "Sarah", "David", "Emily")
-    $userLastName = Get-Random -InputObject @("Doe", "Smith", "Johnson", "Williams", "Brown", "Jones")
-    $userName = $userPrefix + $i
-    $userPassword = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
-    $userDisplayName = $userFirstName + " " + $userLastName
-    $userEmail = $userName + "@" + $domainName
-    $userOU = "OU=$ouName,DC=$domainName"
+# Set variables for OU group creation
+$ouGroupPrefix = "Test"
+$ouGroupCount = 5
+$usercount = 50
+$password = "Pa$$w0rd00@!"
+
+# Generate random users
+for ($i = 1; $i -le $usercount; $i++) {
+    # Set variables for user properties
+    $firstName = Get-Random -InputObject $firstNameList
+    $lastName = Get-Random -InputObject $lastNameList
+    $displayName = "$firstName $lastName"
+    $samAccountName = "$firstName.$lastName"
+    $password = ConvertTo-SecureString -String $password -AsPlainText -Force
+    $userPrincipalName = "$samAccountName@example.com"
     
-    New-ADUser -Name $userName -DisplayName $userDisplayName -SamAccountName $userName -UserPrincipalName $userEmail -Path $userOU -AccountPassword $userPassword -Enabled $true -EmailAddress $userEmail -ErrorAction SilentlyContinue
+    # Create user
+    New-ADUser -Name $displayName `
+        -SamAccountName $samAccountName `
+        -UserPrincipalName $userPrincipalName `
+        -GivenName $firstName `
+        -Surname $lastName `
+        -DisplayName $displayName `
+        -AccountPassword $password `
+        -Path $ouPath `
+        -Enabled $true
 }
 
-Write-Host "Finished creating $numberOfUsers random users in $numberOfOUs OUs." -ForegroundColor Green
+# Verify users were created
+Get-ADUser -Filter * -SearchBase $ouPath
+
+# Generate random OU groups
+for ($j = 1; $j -le $ouGroupCount; $j++) {
+    $groupName = "$ouGroupPrefix$j"
+    $groupPath = "OU=$groupName,$ouPath"
+    New-ADOrganizationalUnit -Name $groupName `
+        -Path $ouPath
+}
+
+# Verify OU groups were created
+Get-ADOrganizationalUnit -Filter "Name -like '$ouGroupPrefix*'" -SearchBase $ouPath
+ 
